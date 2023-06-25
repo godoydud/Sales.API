880e972309247216936a76b1da28457318294c2b
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-06-25 14:27:42 -0400

    Alterando assinatura do metodo

--- Sales.API.Domain/Interfaces/Services/IComissionService.cs
+++ Sales.API.Domain/Interfaces/Services/IComissionService.cs
@@ -1,10 +1,11 @@
 ﻿using Sales.API.Domain.DTOs;
+using Sales.API.Domain.DTOs.Base;
 
 namespace Sales.API.Domain.Interfaces.Services
 {
     public interface IComissionService
     {
-        Task<ComissionDTO> CreateAsync(ComissionDTO comissionDTO);
+        Task<ResponseDTO<ComissionDTO>> CreateAsync(ComissionDTO comissionDTO);
         Task<ICollection<ComissionDTO>> GetAllAsync();
         Task<ComissionDTO> GetByIdAsync(Guid id);
         Task<ComissionDTO> UpdateAsync(ComissionDTO productDTO);
