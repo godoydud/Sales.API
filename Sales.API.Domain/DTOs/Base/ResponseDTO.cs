3881fbd6895a941389648cd66cc26949bfec9455
Eduardo Fuso <eduardo.fuso@sesims.com.br>
2023-06-25 14:27:18 -0400

    Criado classe ResponseDTO para padronizar respostas da API

---
+++ Sales.API.Domain/DTOs/Base/ResponseDTO.cs
@@ -0,0 +1,50 @@
+﻿namespace Sales.API.Domain.DTOs.Base
+{
+    public class ResponseDTO<T>
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public T? Entity { get; set; }
+        public List<string>? ValidationErrors { get; set; }
+
+        public ResponseDTO()
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        public ResponseDTO(int statusCode)
+            : this()
+        {
+            StatusCode = statusCode;
+        }
+
+        public ResponseDTO(int statusCode, string message = default, T entity = default)
+            : this()
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Entity = entity;
+        }
+
+        public ResponseDTO(int statusCode, T entity = default)
+            : this()
+        {
+            StatusCode = statusCode;
+            Entity = entity;
+        }
+
+        public ResponseDTO(int statusCode, T entity = default, List<string> validationErrors = default)
+            : this()
+        {
+            StatusCode = statusCode;
+            Entity = entity;
+            ValidationErrors = validationErrors;
+        }
+
+        public ResponseDTO(int statusCode, List<string> validationErrors, string message = default, T entity = default)
+            : this(statusCode, message)
+        {
+            ValidationErrors = validationErrors;
+        }
+    }
+}
